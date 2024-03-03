5ed32e9e07e31d2b860496d813efe3be8b24d292
grad48 <152669027+grad48@users.noreply.github.com>
2024-03-03 21:33:13 +0200

    Update insult.js

--- script/insult.js
+++ script/insult.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 module.exports.config = {
-  name: "insult",
+  name: "انسولت",
   version: "1.0.0",
   role: 0,
   hasPrefix: true,
-  description: "Get a random insult.",
+  description: "احصل على عشوائي انسولت.",
   usage: "insult",
   credits: "Developer",
   cooldown: 0
@@ -20,8 +20,8 @@ module.exports.run = async ({
   try {
     const response = await axios.get('https://evilinsult.com/generate_insult.php?lang=en&type=json');
     const insult = response.data.insult;
-    api.sendMessage(`Here's a random insult for you: ${insult}`, threadID);
+    api.sendMessage(`هذه إهانة عشوائية لك: ${insult}`, threadID);
   } catch (error) {
-    api.sendMessage("Sorry, I couldn't fetch an insult at the moment. Please try again later.", threadID, messageID);
+    api.sendMessage("آسف، لم أتمكن من جلب الإهانة في الوقت الحالي. الرجاء معاودة المحاولة في وقت لاحق .", threadID, messageID);
   }
 };
