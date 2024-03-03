60980c12a33bcc5862e088df29f0e9b77a094b50
grad48 <152669027+grad48@users.noreply.github.com>
2024-03-03 21:56:51 +0200

    Update unsend.js

--- script/unsend.js
+++ script/unsend.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-  name: "unsend",
+  name: "مسح",
   version: "1.0.0",
   role: 0,
   hasPrefix: true,
-  aliases: ['unsent', 'remove', 'rm'],
-  usage: 'Unsent [reply]',
-  description: "Unsend bot's message",
+  aliases: ['🙂', 'حذف', 'امسح'],
+  usage: 'مسح [رد]',
+  description: "قم بإلغاء إرسال رسالة الروبوت",
   credits: 'Deveploper',
   cooldown: 0
 };
@@ -13,7 +13,7 @@ module.exports.run = async function({
   api,
   event
 }) {
-  if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("I can't unsend from other message.", event.threadID, event.messageID);
-  if (event.type != "message_reply") return api.sendMessage("Reply to bot message", event.threadID, event.messageID);
-  return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage("Something went wrong.", event.threadID, event.messageID) : '');
+  if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage("لا أستطيع إلغاء الإرسال من رسالة أخرى.", event.threadID, event.messageID);
+  if (event.type != "message_reply") return api.sendMessage("الرد على رسالة بوت", event.threadID, event.messageID);
+  return api.unsendMessage(event.messageReply.messageID, err => (err) ? api.sendMessage("هناك خطأ ما.", event.threadID, event.messageID) : '');
 }
