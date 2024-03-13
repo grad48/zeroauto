e312adfa463ccd11ca10e9b5dfcdd0081335eefb
grad48 <152669027+grad48@users.noreply.github.com>
2024-03-13 10:00:30 +0200

    Update flag.js

--- script/flag.js
+++ script/flag.js
@@ -9,37 +9,41 @@ module.exports.config = {
   credits: 'العاب',
   cooldown: 0
 };
-module.exports.run = async function({ api, event,Threads, Users }) {
+
+module.exports.run = async function({ api, event, Users, Currencies }) {
         const axios = global.nodemodule["axios"];
         const fs = global.nodemodule["fs-extra"];
+        var TOKEN = "6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
+        var data = await Currencies.getData(event.senderID);
+        var money = data.money
+        if( money < 500) api.sendMessage(`انت لا تملك المال الكافي لهذا\n\n قم بكتابة هذا الامر لتحصل على بعض المال ${global.config.PREFIX}كهف\n\nصنع بواسطة: benzo 凧`, event.threadID, event.messageID) //thay số tiền cần trừ vào 0, xóa money = 0
+        else {
+        var tile = Math.floor(Math.random() * 101);
+        
 
-        var { participantIDs } =(await Threads.getData(event.threadID)).threadInfo;
-        var tle = Math.floor(Math.random() * 101);
-        var namee = (await Users.getData(event.senderID)).name
-        const botID = api.getCurrentUserID();
-        const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
-        var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-        var name = (await Users.getData(id)).name
+        //let loz = await api.getThreadInfo(event.threadID);
+        var emoji = event.participantIDs;
+        var id = emoji[Math.floor(Math.random() * emoji.length)];
+
+        var namee = (await Users.getData(event.senderID)).name;
+        var name = (await Users.getData(id)).name;
+
         var arraytag = [];
-                arraytag.push({id: event.senderID, tag: namee});
-                arraytag.push({id: id, tag: name});
-
-
-        let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
-            fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
-
-        let gifLove = (await axios.get( `https://i.imgur.com/Uuo2dE4.jpeg`, { responseType: "arraybuffer" } )).data; 
-            fs.writeFileSync( __dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8") );
-
-        let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
-            fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
-
+        arraytag.push({id: event.senderID, tag: namee});
+        arraytag.push({id: id, tag: name});
+      
+        Currencies.setData(event.senderID, options = {money: money - 1000})
+  
+        let Avatar = (await axios.get( `https://graph.facebook.com/${id}/picture?height=720&width=720&access_token=${TOKEN}`, { responseType: "arraybuffer" } )).data; 
+            fs.writeFileSync( __dirname + "/cache/1.png", Buffer.from(Avatar, "utf-8") );
+        let Avatar2 = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?height=720&width=720&access_token=${TOKEN}`, { responseType: "arraybuffer" } )).data;
+            fs.writeFileSync( __dirname + "/cache/2.png", Buffer.from(Avatar2, "utf-8") );
         var imglove = [];
-
-              imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-              imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-              imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-
-        var msg = {body: `🥰لدينا محبوبان هنا\n💌مبروك الزواح\n💕نسبة الرمسنية بينكم هي: ${tle}%\n`+namee+" "+"💓"+" "+name, mentions: arraytag, attachment: imglove}
-        return api.sendMessage(msg, event.threadID, event.messageID)
+              imglove.push(fs.createReadStream(__dirname + "/cache/1.png"));
+              imglove.push(fs.createReadStream(__dirname + "/cache/2.png"));
+        var msg = {body: `لدينا محبوبان هنا\nنسبة الرومنسية: ${tile}%\n`+namee+" "+"💓"+" "+name, mentions: arraytag, attachment: imglove}
+        return api.sendMessage(msg, event.threadID, event.messageID);
+        //fs.unlinkSync(__dirname + '/cache/1.png');
+        //fs.unlinkSync(__dirname + '/cache/2.png');
+      }
           }
